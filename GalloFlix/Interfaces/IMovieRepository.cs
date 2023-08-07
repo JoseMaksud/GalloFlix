f4cbf4c6cd71a79f79d620d644bd74a8898841cf
José Francisco Dos Santos Neto <jf9718830@gmail.com>
2023-08-07 07:46:37 -0300

    Adicionando detalhes aos filmes

--- GalloFlix/Interfaces/IMovieRepository.cs
+++ GalloFlix/Interfaces/IMovieRepository.cs
@@ -1,6 +1,10 @@
 using GalloFlix.Models;
+
 namespace GalloFlix.Interfaces;
 
 public interface IMovieRepository : IRepository<Movie>
 {
+    List<Movie> ReadAllDetailed();
+
+    Movie ReadByIdDetailed(int id);
 }
